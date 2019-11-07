02a11e35f6d08fab52c2c0449bbca9a84d456720
cengamat <7.horcruxe@windowslive.com>
2019-11-07 22:15:27 +0300

    React Router

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -2,20 +2,36 @@ import React, { Component } from "react";
 import Navbar from "./components/Navbar";
 import Users from "./components/Users";
 import AddUser from "./components/AddUser";
-
+import { BrowserRouter as Router, Route } from "react-router-dom"
 import "./App.css";
+
+const Home = () => {
+  return (
+    <h3>Home Page</h3>
+  )
+}
+
+const About = () => {
+  return (
+    <h3>About Page</h3>
+  )
+}
 
 class App extends Component {
 
   render() {
     return (
-      <div className="container">
-        <Navbar title="User App" />
-        <hr />
-        <AddUser />
+      <Router>
+        <div className="container">
+          {/* <Route exact path="/" component={Home} />
+          <Route exact path="/about" component={About} /> */}
 
-        <Users />
-      </div>
+          <Navbar title="User App" />
+          <hr />
+          <Route exact path="/" component={Users} />
+          <Route exact path="/add" component={AddUser} />
+        </div>
+      </Router>
     );
   }
 }
